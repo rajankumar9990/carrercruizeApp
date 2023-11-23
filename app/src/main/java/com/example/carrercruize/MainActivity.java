a671979f636653e0376fa050b65c8378b799984d
Rajan <guptarajanapex99@gmail.com>
2023-11-23 14:04:24 +0530

    thrid commit

--- app/src/main/java/com/example/carrercruize/MainActivity.java
+++ app/src/main/java/com/example/carrercruize/MainActivity.java
@@ -2,17 +2,33 @@ package com.example.carrercruize;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button login= findViewById(R.id.button);
-    Button signinbygoogle=findViewById(R.id.button2);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button signup= findViewById(R.id.button);
+        Button signinbygoogle=findViewById(R.id.button2);
+        TextView loginbtn=findViewById(R.id.textView7);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,login_page.class));
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,signup_page.class));
+            }
+        });
     }
 }
