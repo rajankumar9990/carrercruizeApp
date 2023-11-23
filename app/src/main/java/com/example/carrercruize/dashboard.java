92c205ebec8f74878701c60b727ebb1d5d63877e
Aashish <ashishkuchhal68@gmail.com>
2023-11-23 20:49:49 +0530

    5th commit by asshish

--- app/src/main/java/com/example/carrercruize/dashboard.java
+++ app/src/main/java/com/example/carrercruize/dashboard.java
@@ -1,8 +1,19 @@
 package com.example.carrercruize;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class dashboard extends AppCompatActivity {
 
@@ -10,5 +21,63 @@ public class dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ImageView menuIcon = findViewById(R.id.menu_icon);
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
+
+        //bottom navigation
+        BottomNavigationView bottomNavigationView=findViewById (R.id.bottomNavigationView);
+        home_fragment homeFragment=new home_fragment ();
+        job_fragment jobFragment=new job_fragment ();
+        profile_fragment profileFragment=new profile_fragment ();
+        getSupportFragmentManager ().beginTransaction ().replace (R.id.container,homeFragment).commit ();
+
+        bottomNavigationView.setOnItemSelectedListener (new NavigationBarView.OnItemSelectedListener ( ) {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId ()){
+                    case R.id.homenavigate:
+                        getSupportFragmentManager ().beginTransaction ().replace (R.id.container,homeFragment).commit ();
+                        return true;
+                    case R.id.jobnavigate:
+                        getSupportFragmentManager ().beginTransaction ().replace (R.id.container,homeFragment).commit ();
+                        return true;
+                    case R.id.profilenavigate:
+                        getSupportFragmentManager ().beginTransaction ().replace (R.id.container,homeFragment).commit ();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+    private void showPopupMenu(View v) {
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        MenuInflater inflater = popupMenu.getMenuInflater();
+        inflater.inflate(R.menu.menu2, popupMenu.getMenu());
+
+        // Set the click listener for each menu item
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch(item.getItemId ()){
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        popupMenu.show();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 }
---
+++ app/src/main/java/com/example/carrercruize/job_fragment.java
@@ -0,0 +1,21 @@
+package com.example.carrercruize;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class job_fragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate (R.layout.fragment_job_fragment, container, false);
+    }
+}
